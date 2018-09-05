76f2e626cc6a316934642f3f651fba8f568a9734
jimsweeney <jimsweeney@jims-mbp-2017.jetpack>
2018-09-05 14:46:11 -0400

    Initial commit

---
+++ PSAP_DB/src/main/java/com/PSAP/PsapDbApplication.java
@@ -0,0 +1,12 @@
+package com.PSAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsapDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsapDbApplication.class, args);
+	}
+}
